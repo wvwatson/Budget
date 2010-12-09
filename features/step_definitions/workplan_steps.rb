782cf970ad87fa5dda2e6cd34301998c111031a4
Wavell <watson@preferit.com>
2010-12-09 15:07:47 -0600

    finished off the scenarios for the workplan

--- features/step_definitions/workplan_steps.rb
+++ features/step_definitions/workplan_steps.rb
@@ -14,3 +14,20 @@ Then /^the result should be "([^"]*)" and "([^"]*)" on the screen$/ do |arg1, ar
   pending # express the regexp above with the code you wish you had
 end
 
+Given /^I have entered "([^"]*)" and assigned it to the "([^"]*)" goal as the first action$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have entered "([^"]*)" and assigned it to the "([^"]*)" goal as the second action$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I display the "([^"]*)" goal$/ do |arg1| 
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the result should be "([^"]*)" and "([^"]*)" on the screen for the "([^"]*)" goal$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+
