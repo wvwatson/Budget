a57e9d87b7bbace1bf450926e1a6e0eb0ce7fce2
wavell watson <watson@Macintosh-62.local>
2010-12-09 12:32:32 -0600

    removed all of the prerelease spec bs

---
+++ features/step_definitions/workplan_steps.rb
@@ -0,0 +1,16 @@
+Given /^I have entered "([^"]*)" as the first goal$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have entered "([^"]*)" as the second goal$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I press show workplan$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the result should be "([^"]*)" and "([^"]*)" on the screen$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
--- features/workplan.feature
+++ features/workplan.feature
@@ -2,3 +2,9 @@ Feature: Work
   In order to create a workplan
   As an executive
   I want to create a list of goals and action steps to complete those goals
+
+  Scenario: Add some goals
+    Given I have entered "Lower costs by 10%" as the first goal
+	And I have entered "Increase Income by 5%" as the second goal
+    When I press show workplan
+    Then the result should be "Lower costs by 10%" and "Increase Income by 5%" on the screen
