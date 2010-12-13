53cdd40723071842a53d772f56b62e5662b95f57
Wavell <watson@preferit.com>
2010-12-13 13:56:49 -0600

    hmm watch out for single versus double equals sign

--- features/step_definitions/workplan_steps.rb
+++ features/step_definitions/workplan_steps.rb
@@ -23,36 +23,44 @@ end
 
 Given /^I have entered "([^"]*)" and assigned it to the "([^"]*)" goal as the first action$/ do |arg1, arg2|
   #pending # express the regexp above with the code you wish you had
-    @goals = @workplan.show_workplan
+  @goals = @workplan.show_workplan
+  @goals.each { |goal| puts goal.description }
     @goals.each do | goal | 
-	  # puts "goal desc = " + goal.description
-	  # puts "arg2 = " + arg2
       (goal.addactions arg1) if goal.description == arg2
-	  #@actions = goal.show_actions
-	  #puts @actions.count.to_s
-	  #@actions.each { |action| puts action.description }
     end
 end
 
 Given /^I have entered "([^"]*)" and assigned it to the "([^"]*)" goal as the second action$/ do |arg1, arg2|
   #pending # express the regexp above with the code you wish you had
   @goals = @workplan.show_workplan
+    @goals.each { |goal| puts goal.description }
    @goals.each do | goal | 
      goal.addactions arg1 if goal.description == arg2
    end
 end
 
 When /^I display the "([^"]*)" goal$/ do |arg1| 
-  #pending # express the regexp above with the code you wish you had
    @goals = @workplan.show_workplan
+    @goals.each { |goal| puts goal.description }
    @goals.each do | goal | 
          @actions = goal.show_actions
-		 @actions.each { |action| puts action.description } if goal.description = arg1
-      end
+		 @actions.each { |action| puts action.description } if goal.description == arg1
+    end
 end
 
 Then /^the result should be "([^"]*)" and "([^"]*)" on the screen for the "([^"]*)" goal$/ do |arg1, arg2, arg3|
-  pending # express the regexp above with the code you wish you had
+   @goals = @workplan.show_workplan
+   @goals.each { |goal| puts goal.description }
+   @goals.each do | goal | 
+		if goal.description == arg3
+			@actions = goal.show_actions
+			@actions.each { |action| puts action.description } 
+			if goal.description = arg3
+			  @actions[0].description.should ==  arg1
+			  @actions[1].description.should ==  arg2
+			end
+		end
+    end
 end
 
 
