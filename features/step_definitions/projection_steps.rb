1c682c76a3a3e1f4d57b5e185236a2d654a3c3d9
wavell watson <watson@matt-touch.tea.state.tx.us>
2010-12-30 12:51:03 -0600

    project example

---
+++ features/step_definitions/projection_steps.rb
@@ -0,0 +1,26 @@
+# regular people want to use the word 'bill' as synonym for expense
+
+expenses do
+
+  every month do
+    rent $750
+    car_note $350
+  end
+
+  every 6.months do
+    car_insurance $600
+  end
+
+  # one time charge
+  on 8/3/2011 do
+    birthday $250
+  end
+  
+  # specialized calculations
+  every month do
+    rent $750
+    car_note $350
+  end
+
+end
+
