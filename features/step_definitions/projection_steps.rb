aef327b8b39a00ade94d610dd07284869bf15d1b
Wavell <watson@preferit.com>
2010-12-30 13:20:12 -0600

    more projection pseudo code

--- features/step_definitions/projection_steps.rb
+++ features/step_definitions/projection_steps.rb
@@ -1,7 +1,10 @@
 # regular people want to use the word 'bill' as synonym for expense
+# maybe have a file named expenses/income so that it doesn't have to be set.
+
 
 expenses do
 
+  # monthly context (monthly is default period)
   every month do
     rent $750
     car_note $350
@@ -15,12 +18,28 @@ expenses do
   on 8/3/2011 do
     birthday $250
   end
+  # or
+  birthday $250, date => 8/3/2011
+  
+  # fudge expenses 
+  car_maintenance do
+    cost $500
+	chance 25%
+  end
+  # or
+  car_maintenance $500, chance => 25%
+  
   
   # specialized calculations
-  every month do
-    rent $750
-    car_note $350
+  on 4/15/2011 do
+    taxes $5000 do
+		# whatever ruby code you want in here, with access to context
+		if cash_on_hand < 10000
+			file_extension
+			date = date.3.months.from
+		end
+	end
   end
-
+  
 end
 
