9739e782facf5d7c1517c2aba406b1b6bef3ad1f
wavell watson <watson@Macintosh-62.local>
2011-01-15 13:42:54 -0600

    fixed expenese total

--- features/step_definitions/export_finance_excel_step.rb
+++ features/step_definitions/export_finance_excel_step.rb
@@ -3,8 +3,8 @@ Before do
 end
 
 Given /^I have income and expenses calculated for (\d+) year$/ do |arg1|
-    pending # express the regexp above with the code you wish you had
-  #@my.export_excel
+    #pending # express the regexp above with the code you wish you had
+  @my.export_excel
 end
 
 When /^I export the income statement to excel$/ do
