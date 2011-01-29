a20c07018d7757bf8890f313ca64f8212245aa1c
wavell watson <watson@Macintosh-62.local>
2011-01-29 09:36:36 -0600

    mongodb

---
+++ features/step_definitions/ExportFinanceMongodb_steps.rb
@@ -0,0 +1,11 @@
+When /^I export the expenses to mongodb$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a mongo database should be created$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the mongodb database should have (\d+) columns filled$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
