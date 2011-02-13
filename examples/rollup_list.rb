4f7b2a00d985db6ec69e7c2a0cac59a6c797844b
wavell watson <watson@Macintosh-62.local>
2011-02-12 23:54:42 -0600

    updated rollup list example

---
+++ examples/rollup_list.rb
@@ -0,0 +1,11 @@
+# applies to everything that gets rolled up from marketing
+from :marketing do
+end
+
+# applies to everything that gets rolled into finance
+to :finance do
+end
+
+# applies when rolling from help_desk into information_technology
+from :help_desk, to: :information_technology do
+end
--- lib/rollup.rb
+++ lib/rollup.rb
@@ -40,7 +40,7 @@ module RollupBuilder
   #  (executive (finance (budgeting payroll)) 
   #             (information_technology (helpdesk operations software_development)) 
   #              marketing sales)
-  attr_accessor :tree_list
+
   
   
   def addrule(child)
@@ -48,43 +48,135 @@ module RollupBuilder
   end
   
   
-  def rollup_loop
+  # add a node to the node list and the path list
+  def add_rollup(name, *args, &block)
+    
+    # @rollup_rule_list.each do |rollup|
+    #   if rollup.name == name
+    #     return # exists already
+    #   end
+    # end
+    #debugger
+    # node = self.const_get(node_class).new # need to instantiate the including class somehow
+    # node = @node_class.new
+    # node.name = name 
+
+    # if args
+      # @time = args[0][:reports_to] if args[0].is_a?(Hash) 
+    # end
+    
+    if args[0].is_a?(Hash) # syntax field_sales reports_to: :sales
+			# debugger
+      node.parent_name = args[0][:reports_to].to_s
+    elsif node.parent_name == nil # syntax marketing :graphic_design
+       node.name = name
+       node.parent_name = args[0].to_s
+		else
+      node.parent_name = @parent_stack.last
+      node
+    end
+      # expense.amount = amount
+      #      expense.period = @period
+
+	  # 
+	  #   case @period
+	  #   when :one_time
+	  #     #debugger
+	  #     expense.date=@one_time_date
+	  #   when :incremental
+	  #     expense.date=@increment
+	  #   # when :ranged
+	  #   #      # fix this to create expenses per period type for the range
+	  #   #      expense.date=@ranged_start_date
+	  #   end
+	  #   expense.range_start_date=@range_start_date
+	  #   expense.range_end_date=@range_end_date
+	  #   expense.ranged=@ranged
+	  #        
+	    @node_list.push(node)
+	  #   # run block before adding that code to the expense
+      # yield if block_given?
+	  #   @expense_list.last.custom_code=block
+	  #   #cleanup here?
+  end
+
+
+  # dynamically define expenses
+  def method_missing(methId, *args, &block)
+    #debugger
+    # maybe make more secure/easier to debug by requiring method to have 
+    # prefix of exp_ or rev_
+    str = methId.id2name
+    #debugger
+    #debugger if str == 'taxes'
+    #need to ensure 1 variable
+    if args.count == 0 
+      #debugger
+      add_node(str)
+      @parent_stack.push(str) if block_given?
+      instance_eval &block if block_given? # needs to call with an arg
+      @parent_stack.pop if block_given?
+      # debugger
+      #puts "after pop"
+    elsif args.count == 1 
+      # debugger
+      add_node(str, *args, &block)
+    end
+    #add define method here
+  end  
+  
+  
+  def load_node(filelocation="/../../rollup_list.rb")
+    # @period = :monthly
+    # @ranged = nil
+    contents = File.open(filelocation, 'rb') { |f| f.read }
+    self.instance_eval contents
+    # debugger
+    #     puts "I am here"
   end
   
-  def execute_rollup (current_object, parent_object, rule)
+  # def initialize
+  #   @rollup_list={}
+  # end
+end
+
+module rollup
+  
+  attr_accessor :tree_list #node_list from hierarchy
+  
+  def rollup_object (current_object, parent_object, rule)
   end
   
   # should we use memoization?
   def rollup
     # loop through children from bottom
     # designate current object and current parent
-    
-    # make a function to handle each rule e.g. execute_rollup
-    
-		# loop through the rollup rules
-    @rollup_rule_list.each do |rule|
-      
-    end
+    @tree_list.each do |node|
 
-		# there are three kinds of matches
-    #   1) a 'from' match is when the current object is the 'from' object when
-		# 	  there is no 'to' object (wildcard)
-		# 	2) a 'to' match is when the current parent is the 'to' object when
-		#			there is not 'from object (wildcard)
-		#		3) a full match is when the current object and parent match the
-		#			'from' and 'to' objects respectively
-		# when there is a match call the before rollup logic, then rollup logic,
-		# 	then the after rollup logic
-		# possibly implement before_all_children/after_all_children
-    # when no more children accumulate current object's content
+      # make a function to handle each rule e.g. execute_rollup
+    
+  		# loop through the rollup rules
+      @rollup_rule_list.each do |rule|
+        # pass the real object?
+        rollup_object(node.name, node.parent_name, rule)
+      end
 
-		# possibly create a new control that acts like fold (inject)
-		#  and passes in the object currently being rolled up also
-		#  processes the objects in an order based on what the objects have listed
-		#  in their predefined order attribute.
-  end
-  
-  def initialize
-    @rollup_list={}
+  		# there are three kinds of matches
+      #   1) a 'from' match is when the current object is the 'from' object when
+  		# 	  there is no 'to' object (wildcard)
+  		# 	2) a 'to' match is when the current parent is the 'to' object when
+  		#			there is not 'from object (wildcard)
+  		#		3) a full match is when the current object and parent match the
+  		#			'from' and 'to' objects respectively
+  		# when there is a match call the before rollup logic, then rollup logic,
+  		# 	then the after rollup logic
+  		# possibly implement before_all_children/after_all_children
+      # when no more children accumulate current object's content
+
+  		# possibly create a new control that acts like fold (inject)
+  		#  and passes in the object currently being rolled up also
+  		#  processes the objects in an order based on what the objects have listed
+  		#  in their predefined order attribute.
+		end
   end
 end
